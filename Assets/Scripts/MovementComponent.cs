e654f211db8a7a61305ba29d21dc4a596e32bfbb
IgorRukan <vasyapro1@bk.ru>
2023-12-09 14:55:18 +0300

    MovementComponent Script
    Добавлен MovementComponent script

---
+++ Assets/Scripts/MovementComponent.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementComponent : MonoBehaviour
+{
+    public float speed = 3f;
+    public CharacterController characterController;
+    public float gravity = -9.81f;
+
+    private CharacterController Controller
+    {
+        get { return characterController = characterController ? characterController : GetComponent<CharacterController>(); }
+    }
+
+
+    public void Move(Vector3 movement)
+    {
+        movement.y = gravity;
+        Vector3 displacement = movement * speed * Time.deltaTime;
+        Controller.Move(displacement);
+    }
+}
