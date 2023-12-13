426c54f456fa2e3b8c667d8582481582e3151988
IgorRukan <vasyapro1@bk.ru>
2023-12-13 21:35:03 +0300

    Portal

---
+++ Assets/Scripts/Level/Portal.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+   
+    public string nextLevel;
+
+    private void Start()
+    {
+        gameObject.SetActive(false);  
+    }
+    private void ActivateTeleport()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(nextLevel);
+    }
+}
